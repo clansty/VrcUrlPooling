d0d48e5fc534c08324f7e97d8a345ce981f875b2
Clansty <i@gao4.pw>
2025-05-24 18:54:02 +0800

    string length

--- VrcUrlPooling/UrlSlot.cs
+++ VrcUrlPooling/UrlSlot.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace VrcUrlPooling;
 
 [Index(nameof(Id), IsUnique = true)]
-[Index(nameof(Url))]
+[Index(nameof(Url), IsUnique = false)]
 public abstract class UrlSlotBase
 {
     public int Id { get; set; }
+    [Column(TypeName = "varchar(5000)")]
     public string? Url { get; set; }
     public DateTime? ExpiresAt { get; set; }
 }
